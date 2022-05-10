6c4c22788c6d0323fa0088746bd84f115c1a428a
pixelbin-dev <101859050+pixelbin-dev@users.noreply.github.com>
2022-05-10 12:32:38 +0530

    v1.0.2 (#4)
    * added support for all version of axios
    
    * fixed corner cases for url utils
    
    Co-authored-by: Noel Mathew <noelmathew@gofynd.com>

--- utils/common.utils.js
+++ utils/common.utils.js
@@ -1,8 +1,5 @@
 import { PDKInvalidUrlError } from "../errors/PixelbinErrors";
-
-const version2Regex = /^v[1-2]$/;
-const urlWithZone = /^\/(.*)\/([a-zA-Z0-9_-]{6})\/(.+)\/(.*)$/;
-const urlWithoutZone = /\/(.*)\/(.+)\/(.*)/;
+import { version2Regex, urlWithZone, urlWithoutZone } from "./regex";
 
 const getUrlParts = function (pixelbinUrl) {
     const { protocol, host, pathname, search } = new URL(pixelbinUrl);
@@ -18,12 +15,12 @@ const getUrlParts = function (pixelbinUrl) {
     }
     if (parts[1].length < 3)
         throw new PDKInvalidUrlError("Invalid pixelbin url. Please make sure the url is correct.");
-    if (urlWithZone.test(pathname)) {
+    if (urlWithZone.test(parts.join("/"))) {
         urlDetails["cloudName"] = parts.splice(1, 1)[0];
         urlDetails["zoneSlug"] = parts.splice(1, 1)[0];
         urlDetails["pattern"] = parts.splice(1, 1)[0];
         urlDetails["filePath"] = parts.slice(1).join("/");
-    } else if (urlWithoutZone.test(pathname)) {
+    } else if (urlWithoutZone.test(parts.join("/"))) {
         urlDetails["cloudName"] = parts.splice(1, 1)[0];
         urlDetails["pattern"] = parts.splice(1, 1)[0];
         urlDetails["filePath"] = parts.slice(1).join("/");
