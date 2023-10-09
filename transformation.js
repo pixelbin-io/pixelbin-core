a19d9e577d0e17bcae461d739c9f8010b289770e
Krithik Suvarna <krithiksuvarna@gofynd.com>
2023-10-09 11:40:56 +0530

    v5.2.2

--- transformation.js
+++ transformation.js
@@ -1,20 +1,20 @@
 import { PDKIllegalArgumentError } from "./errors/PixelbinErrors";
 
 /**
- * class to create a Transformation object
+ * Class to create a Transformation object
  */
 class Transformation {
     /**
-     * @param {Transformation} _transformation
+     * @param {Array<Object>} _transformation - Array of transformation operations.
      */
     constructor(_transformation) {
         this._transformation = _transformation || [];
     }
 
     /**
-     * provides functionality to append transformation.
-     * @param {Transformation} transformation.
-     * returns Transformation
+     * Provides functionality to append a transformation.
+     * @param {Transformation} transformation - Transformation to append.
+     * @returns {Transformation}
      */
     pipe(transformation) {
         if (transformation instanceof Transformation) {
@@ -27,22 +27,22 @@ class Transformation {
     }
 
     /**
-     * provides functionality to get all transformation.
-     * returns Transformation list
+     * Provides functionality to get all transformations.
+     * @returns {Array<Object>} - Transformation list.
      */
     getTransformation() {
         return this._transformation;
     }
 
     /**
-     * allows the user to generate a custom transformation with the input parameters.
-     * @param {Object} arg - custom transformation
-     * @param {string} arg.plugin - transformation plugin
-     * @param {string} arg.name - transformation name
-     * @param {Array<Object>} arg.values - transformation parameter object
-     * @param {string} [arg.values[].key] - transformation parameter key
-     * @param {string} [arg.values[].value] - transformation parameter value
-     * returns Transformation
+     * Allows the user to generate a custom transformation with the input parameters.
+     * @param {Object} arg - Custom transformation.
+     * @param {string} arg.plugin - Transformation plugin.
+     * @param {string} arg.name - Transformation name.
+     * @param {Array<Object>} arg.values - Transformation parameter object.
+     * @param {string} [arg.values[].key] - Transformation parameter key.
+     * @param {string} [arg.values[].value] - Transformation parameter value.
+     * @returns {Transformation}
      */
     static customTransformation({ plugin, name, values = [] }) {
         if (!(values instanceof Array))
